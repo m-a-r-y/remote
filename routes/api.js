b7cded15dd8bffa7a447553550a75606ff414f05
Mary Le <maaaryle@gmail.com>
2014-09-11 16:08:23 -0700

    ore api send/receive/extract

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,26 @@ var express = require("express");
 
 var router = express.Router();
 
+
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGOHQ_URL);
+
+
+var peopleInfo = new mongoose.Schema({
+	code: String,
+    name: String,
+    email: String,
+    phone: String,
+    comments: String
+});
+
+var Info = mongoose.model('Info', peopleInfo);
+
+//var testi1 = new Info( { name: 'Bob Smith', email: 'b@s.com', phone: '000-000-9999', comments: 'hi'} );
+
+//testi1.save();
+
+
 router.get("/test", function(req, res){
 	res.json({method: 'GET', serverTime: new Date()});
 });
@@ -17,14 +37,46 @@ router.get("/test", function(req, res){
 // });
 
 
-router.post("/test", function(req, res){
+// router.post("/test", function(req, res){
 	
-	var output = req.body;
-	output['method'] = "POST";
-	output['serverTime'] = new Date();
-	output['numberKeysInData'] = Object.keys(req.body).length;
+// 	var output = req.body;
+// 	output['method'] = "POST";
+// 	output['serverTime'] = new Date();
+// 	output['numberKeysInData'] = Object.keys(req.body).length;
 	
-	res.json(output);
+// 	res.json(output);
+// });
+
+router.post("/info", function(req, res){
+	var info = new Info({ 
+		code: (new Date()).getTime().toString(),
+		name:req.body.name, 
+		email:req.body.email, 
+		phone:req.body.phone, 
+		comments:req.body.comments 
+	});
+	
+	info.save(function(err, postedInfo){
+		res.json(postedInfo);
+	});
 });
+
+router.get("/info", function(req, res){
+	Info.find({}).exec(function(err, result){
+		res.json(result);
+	});
+});
+
+router.get("/info/:infoCode", function(req, res){
+	Info.findOne({ code:req.params.infoCode }).exec(function(err, info){
+		if (info) {
+			res.json(info);
+		} else {
+			res.json(404, {error: "Nothing found"});
+		}
+	});
+});
+
+
 
 module.exports = router;
