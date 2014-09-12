9da6fd34e820649366c67ab79568fa4cbc7a9210
Mary Le <maaaryle@gmail.com>
2014-09-12 12:56:51 -0700

    restaurant Schema portion updated

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,10 @@ var express = require("express");
 
 var router = express.Router();
 
-
 var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGOHQ_URL);
 
-// ----- User Info --------//
+// ----- User Info Schema --------//
 var peopleInfo = new mongoose.Schema({
 	code: String,
     name: String,
@@ -17,48 +16,7 @@ var peopleInfo = new mongoose.Schema({
 
 var Info = mongoose.model('Info', peopleInfo);
 
-//var testi1 = new Info( { name: 'Bob Smith', email: 'b@s.com', phone: '000-000-9999', comments: 'hi'} );
-
-//testi1.save();
-
-
-// ----------- Restaurant ------------ //
-var restaurant = new mongoose.Schema({
-	code: String,
-    restaurant: String
-});
-
-var Rest = mongoose.model('Rest', restaurant);
-
-//------test sample------//
-// router.get("/test", function(req, res){
-// 	res.json({method: 'GET', serverTime: new Date()});
-// });
-
-// router.post("/test", function(req, res){
-// 	var output = { method: 'POST', serverTime: new Date() };
-// 	// ['varA', 'varB', 'varC']
-// 	Object.keys(req.body).forEach(function(key) {
-// 		output[key] = req.body[key];
-// 	// output ['varA'] = req.body['varA'];
-// 	});
-// 	res.json(output);
-// });
-
-
-// router.post("/test", function(req, res){
-	
-// 	var output = req.body;
-// 	output['method'] = "POST";
-// 	output['serverTime'] = new Date();
-// 	output['numberKeysInData'] = Object.keys(req.body).length;
-	
-// 	res.json(output);
-// });
-
-
-// --------------- UserInfo ----------------//
-
+//------------- User Info Schema -------------//
 router.post("/info", function(req, res){
 	var info = new Info({ 
 		code: (new Date()).getTime().toString(),
@@ -89,14 +47,20 @@ router.get("/info/:infoCode", function(req, res){
 	});
 });
 
+// ----------- Restaurant Schema ------------ //
+var restaurantSchema = new mongoose.Schema({
+	code: String,
+    restaurantName: String
+});
 
+var REST = mongoose.model('rest', restaurantSchema);
 
 // ---------------- Restaurants ----------------- //
 
 router.post("/restaurant", function(req, res){
-	var restaurant = new Rest({ 
+	var restaurant = new REST({ 
 		code: (new Date()).getTime().toString(),
-		restaurant:req.body.restaurant
+		restaurantName: req.body.restaurantName
 	});
 	
 	restaurant.save(function(err, postedRest){
@@ -105,13 +69,13 @@ router.post("/restaurant", function(req, res){
 });
 
 router.get("/restaurant", function(req, res){
-	Rest.find({}).exec(function(err, result){
+	REST.find({}).exec(function(err, result){
 		res.json(result);
 	});
 });
 
 router.get("/restaurant/:restCode", function(req, res){
-	Restaurant.findOne({ code:req.params.restCode }).exec(function(err, restaurant){
+	REST.findOne({ code:req.params.restCode }).exec(function(err, restaurant){
 		if (restaurant) {
 			res.json(restaurant);
 		} else {
@@ -120,5 +84,78 @@ router.get("/restaurant/:restCode", function(req, res){
 	});
 });
 
+// // ---------------- Menu Items ----------------- //
+// // --------- Menu Items -----------//
+// var menuItemsSchema = new mongoose.Schema({
+// 	code: String,
+//     itemName: String
+// });
+
+// var Items = mongoose.model('Items', menuItems);
+
+// //-----------------------------------//
+
+// router.post("/menuitems", function(req, res){
+// 	var menuitems = new Items({ 
+// 		code: (new Date()).getTime().toString(),
+// 		menuitems:req.body.menuitems
+// 	});
+	
+// 	menuitems.save(function(err, postedRest){
+// 		res.json(postedRest);
+// 	});
+// });
+
+// router.get("/menuitems", function(req, res){
+// 	Items.find({}).exec(function(err, result){
+// 		res.json(result);
+// 	});
+// });
+
+// router.get("/menuitems/:itemCode", function(req, res){
+// 	menutItems.findOne({ code:req.params.itemCode }).exec(function(err, menuitems){
+// 		if (menuitems) {
+// 			res.json(menuitems);
+// 		} else {
+// 			res.json(404, {error: "Nothing found"});
+// 		}
+// 	});
+// });
+
 
 module.exports = router;
+
+//var testi1 = new Info( { name: 'Bob Smith', email: 'b@s.com', phone: '000-000-9999', comments: 'hi'} );
+
+//testi1.save();
+
+//------test sample------//
+// router.get("/test", function(req, res){
+// 	res.json({method: 'GET', serverTime: new Date()});
+// });
+
+// router.post("/test", function(req, res){
+// 	var output = { method: 'POST', serverTime: new Date() };
+// 	// ['varA', 'varB', 'varC']
+// 	Object.keys(req.body).forEach(function(key) {
+// 		output[key] = req.body[key];
+// 	// output ['varA'] = req.body['varA'];
+// 	});
+// 	res.json(output);
+// });
+
+
+// router.post("/test", function(req, res){
+	
+// 	var output = req.body;
+// 	output['method'] = "POST";
+// 	output['serverTime'] = new Date();
+// 	output['numberKeysInData'] = Object.keys(req.body).length;
+	
+// 	res.json(output);
+// });
+
+
+
+
+
